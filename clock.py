fe5fb3bd1679de65824f39ca1339f110ef6ef0a0
rapicastillo <rapi.castillo@gmail.com>
2018-10-11 01:11:45 -0400

    Clock

--- clock.py
+++ clock.py
@@ -8,7 +8,7 @@ from worker import conn
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=30)
+@sched.scheduled_job('interval', minutes=15)
 def timed_for_task():
     q = Queue(connection=conn)
     result = q.enqueue(maintask.run, timeout=500)
